9f994e2329f6d8adb6a2fae72d45c4206b82b02f
Piyush1532 <piyushadav503@gmail.com>
2025-03-27 13:50:56 +0530

    added filter on category

--- frontend/src/Components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/Components/FoodDisplay/FoodDisplay.jsx
@@ -12,7 +12,10 @@ const {food_list}=useContext(StoreContext)
       <h2>Top Dishes Near You</h2>
       <div className="food-display-list">
         {food_list.map((item,index)=>{
-return <FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} price={item.price}/>
+          if (category==="All" || category===item.category) {
+            
+            return <FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} price={item.price}/>
+          }
         })} 
       </div>
     </div>
