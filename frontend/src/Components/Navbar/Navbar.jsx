1f915ca2cf37c93353759303cf33f920a4d3b94c
Piyush1532 <piyushadav503@gmail.com>
2025-03-29 15:03:09 +0530

    updated and added link

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,58 @@
-import React, { useState } from 'react'
-import "./navbar.css"
-import {assets} from "../../assets/assets"
-import {Link} from "react-router-dom"
-const Navbar = () => {
- 
-    const [menu,setMenu]=useState("Home")
-
+import React, { useState } from "react";
+import "./navbar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
+const Navbar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("Home");
 
   return (
-    <div className='navbar'>
-     <img src={assets.logo} alt="" className="logo" />
-     <ul className="navbar-menu">
-        <Link to="/" onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</Link>
-        <a href='#explore-menu' onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</a>
-        <a href='#footer' onClick={()=>setMenu("Contact-us")} className={menu==="Contact-us"?"active":""}>Contact Us</a>
-        <a href='#app-download' onClick={()=>setMenu("Mobile-app")} className={menu==="Mobile-app"?"active":""}>Mobile App</a>
-     </ul>
-     <div className="navbar-right">
+    <div className="navbar">
+      <Link to="/">
+      
+      <img src={assets.logo} alt="" className="logo" />
+      </Link>
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active" : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active" : ""}
+        >
+          Menu
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("Contact-us")}
+          className={menu === "Contact-us" ? "active" : ""}
+        >
+          Contact Us
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("Mobile-app")}
+          className={menu === "Mobile-app" ? "active" : ""}
+        >
+          Mobile App
+        </a>
+      </ul>
+      <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
         <div className="navbar-search-icon">
+          <Link to="/cart">
             <img src={assets.basket_icon} alt="" />
-            <div className='dot'>
-            </div>
+          </Link>
+          <div className="dot"></div>
         </div>
-            <button>Sign In</button>
-     </div>
+        <button onClick={() => setShowLogin(true)}>Sign In</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
