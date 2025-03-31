f6ef70a771c341b8d9989e366f1914137ecd852d
Piyush1532 <piyushadav503@gmail.com>
2025-03-31 14:03:22 +0530

    created total cart amount function

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -19,16 +19,23 @@ const removeFromCart=(itemId)=>{
     setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
 }
 
-useEffect(()=>{
-console.log(cartItems)
-},[cartItems])
+const getTotalCartAmount= () => {
+    let totalAmount=0
+    for(const item in cartItems){
+let itemInfo=food_list.find((product)=>product._id===item)
+totalAmount+=itemInfo.price*cartItems[item]
+    }
+
+    return totalAmount
+}
 
     const contextValue={
 food_list,
 cartItems,
 setCartItems,
 addtoCart,
-removeFromCart
+removeFromCart,
+getTotalCartAmount
     }
     return(
         <StoreContext.Provider value={contextValue}>
