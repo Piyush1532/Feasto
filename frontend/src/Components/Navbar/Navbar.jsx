206a8b2041f96aba57dfbbff550811e77b99f0f6
Piyush1532 <piyushadav503@gmail.com>
2025-03-31 14:02:54 +0530

    added storecontext functon in it

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import "./navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
+import { StoreContext } from "../../Context/storeContext";
 const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("Home");
+  const {getTotalCartAmount}=useContext(StoreContext)
 
   return (
     <div className="navbar">
@@ -47,7 +49,7 @@ const Navbar = ({ setShowLogin }) => {
           <Link to="/cart">
             <img src={assets.basket_icon} alt="" />
           </Link>
-          <div className="dot"></div>
+          <div className={getTotalCartAmount()===0?"":"dot"}></div>
         </div>
         <button onClick={() => setShowLogin(true)}>Sign In</button>
       </div>
