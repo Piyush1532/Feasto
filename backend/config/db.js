54d8ceb2b6c35ae3f4cb0b206413249f89de7400
Piyush1532 <piyushadav503@gmail.com>
2025-04-07 15:31:06 +0530

    corrected typo and spelling

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/Feasto-app`);
+        await mongoose.connect(`${process.env.MONGODB_URL}/FeastoApp`);
         console.log("DB Connected");
     } catch (error) {
         console.error("DB Connection Failed:", error.message);
--- backend/models/FoodModel.js
+++ backend/models/FoodModel.js
@@ -1,13 +1,13 @@
 import mongoose, { Schema } from "mongoose";
 
 const foodSchema=new mongoose.Schema({
-    name:{type:String ,require:true},
-    description:{type:String,require:true},
-    image:{type:String,require:true},
-    category:{type:String,require:true},
-    price:{type:String,require:true},
+    name:{type:String ,required:true},
+    description:{type:String,required:true},
+    image:{type:String,required:true},
+    category:{type:String,required:true},
+    price:{type:String,required:true},
 })
 
-const foodModel=mongoose.models.food ||mongoose.model("food",foodSchema)
+const foodModel=mongoose.models.food ||mongoose.model("foods",foodSchema)
 
 export default foodModel
