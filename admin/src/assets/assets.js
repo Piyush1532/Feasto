33d8e98b645cbd36f39420f52099749d306fa5d0
Piyush1532 <piyushadav503@gmail.com>
2025-04-11 14:35:27 +0530

    admin section files and dependencies

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,16 @@
+import logo from "./logo.png"
+import  add_icon from "./add_icon_white.png"
+import order_icon from "./order-27.png"
+import profile_img from "./user_icon.png"
+import upload_icon from "./upload_icon.webp"
+import parcel_icon from "./parcel_icon.png"
+
+
+export const assets ={
+    logo,
+    add_icon,
+    order_icon,
+    profile_img,
+    upload_icon,
+    parcel_icon
+}
