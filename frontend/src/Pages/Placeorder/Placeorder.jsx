16de4b40797b6eb689d1f55454aff23cf712a5dc
Piyush1532 <piyushadav503@gmail.com>
2025-04-20 16:34:19 +0530

    Updated import file spelling

--- frontend/src/Pages/Placeorder/Placeorder.jsx
+++ frontend/src/Pages/Placeorder/Placeorder.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import "./placeorder.css"
-import { StoreContext } from '../../Context/storeContext'
+import { StoreContext } from '../../Context/StoreContext'
 const Placeorder = () => {
 
 const {getTotalCartAmount}=useContext(StoreContext)
