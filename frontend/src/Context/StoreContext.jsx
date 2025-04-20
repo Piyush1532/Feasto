838a16c641839db927f7e37b8ae5f1d492cb7b8a
Piyush1532 <piyushadav503@gmail.com>
2025-04-20 16:39:02 +0530

    added 2 states and updated

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,5 +1,6 @@
+import axios from "axios";
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
+
 export const StoreContext=createContext(null)
 
 
@@ -7,6 +8,9 @@ const StoreContextProvider=(props)=>{
  
 const [cartItems,setCartItems]=useState({})
 const url=import.meta.env.VITE_BACKEND_URL
+const [token,setToken]=useState("")
+const [food_list,setFoodList]=useState([])
+
 const addtoCart=(itemId)=>{
 if (!cartItems[itemId]) {
     setCartItems((prev)=>({...prev,[itemId]:1}))
@@ -29,6 +33,24 @@ totalAmount+=itemInfo.price*cartItems[item]
     return totalAmount
 }
 
+const fetchFoodList=async (req,res) => {
+   const {data}=await axios.get(url+"/api/food/list")
+   setFoodList(data.data)
+   
+}
+
+
+useEffect(()=>{
+
+async function loadData(params) {
+    await fetchFoodList()
+
+    if (localStorage.getItem("token")) {
+        setToken(localStorage.getItem("token"))   
+       }
+}
+loadData()
+},[])
     const contextValue={
 food_list,
 cartItems,
@@ -36,7 +58,8 @@ setCartItems,
 addtoCart,
 removeFromCart,
 getTotalCartAmount,
-url
+url,
+token,setToken
     }
     return(
         <StoreContext.Provider value={contextValue}>
