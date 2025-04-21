7f064e878a199a8923c0bbd6710667f14eea51b7
Piyush1532 <piyushadav503@gmail.com>
2025-04-21 14:34:32 +0530

    created auth middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+
+const authMiddleware=async (req,res,next) => {
+    const token=req.headers.token
+    console.log(token)
+    if (!token) {
+        return res.json({success:false,message:"Not Aurthorized Login Again"})
+    }
+    try {
+        const tokenDecode=jwt.verify(token,process.env.JWT_SECRET)
+        req.body.userId=tokenDecode.id
+        next()
+    } catch (error) {
+        console.log(error)
+        return res.json({success:false,message:"Error"})
+    }
+}
+
+export default authMiddleware
