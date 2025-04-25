ed5d0e5903c92e585565e51f67aaa0b4dfb25692
Piyush1532 <piyushadav503@gmail.com>
2025-04-25 13:03:30 +0530

    fixed typo

--- frontend/src/Pages/Cart/Cart.jsx
+++ frontend/src/Pages/Cart/Cart.jsx
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react'
 import "./cart.css"
 
-import { StoreContext } from '../../Context/storeContext'
+import { StoreContext } from '../../Context/StoreContext'
 import { useNavigate } from 'react-router-dom'
 const Cart = () => {
 
-  const { cartItems, food_list, removeFromCart, addtoCart,getTotalCartAmount } = useContext(StoreContext)
+  const { cartItems, food_list, removeFromCart, addtoCart,getTotalCartAmount ,url} = useContext(StoreContext)
 
 
   const navigate=useNavigate()
@@ -28,7 +28,7 @@ const Cart = () => {
             return (
               <div>
                 <div className='cart-items-title cart-items-item'>
-                  <img src={item.image} alt="" />
+                  <img src={url+"/images/"+item.image} alt="" />
                   <p>{item.name}</p>
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
