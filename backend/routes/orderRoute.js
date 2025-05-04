636610fe8d79fb1943580cc6632e5f3bda919f6c
Piyush1532 <piyushadav503@gmail.com>
2025-05-04 13:22:23 +0530

    created verify and userorders routes

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,11 @@
 import express from "express"
 import authMiddleware from "../middleware/authMiddleware.js"
-import { placeOrder } from "../controllers/orderController.js"
+import { placeOrder, userOrders, verifyOrder } from "../controllers/orderController.js"
 
 const orderRouter=express.Router()
 
 orderRouter.post("/placeorder",authMiddleware,placeOrder)
+orderRouter.post("/verify",verifyOrder)
+orderRouter.post("/userorders",authMiddleware,userOrders)
 
 export default orderRouter
