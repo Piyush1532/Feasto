867b64467a6e5fc5d8df6b4866962f5082973857
Piyush1532 <piyushadav503@gmail.com>
2025-05-08 13:58:40 +0530

    created listorders function

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -90,4 +90,14 @@ res.json({success:true,data:orders})
 
 
 
-export { placeOrder ,verifyOrder,userOrders};
+const listOrders=async (req,res) => {
+  try {
+    const orders=await orderModel.find({})
+    res.json({success:true,data:orders})
+  } catch (error) {
+    console.log(error)
+    res.json({success:false,message:"Error"})
+  }
+}
+
+export { placeOrder ,verifyOrder,userOrders,listOrders};
