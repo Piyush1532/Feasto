367770567a316ba216bad7eb00896647ae1d3d4d
Piyush1532 <piyushadav503@gmail.com>
2025-05-08 13:59:54 +0530

    created orders page and created fetchallorders function which calls the api

--- admin/src/Pages/Orders/Orders.jsx
+++ admin/src/Pages/Orders/Orders.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
-import "./order.css"
-const Orders = () => {
-  return (
-    <div>
-      order
-    </div>
-  )
-}
+import React, { useEffect, useState } from 'react';
+import './order.css';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { assets } from '../../assets/assets';
 
-export default Orders
+const Orders = () => {
+  const url = import.meta.env.VITE_API_URL;
+  const [orders, setOrders] = useState([]);
+
+  const fetchAllOrders = async () => {
+    try {
+      const response = await axios.get(url + "/api/order/list");
+      if (response.data.success) {
+        setOrders(response.data.data);
+        console.log(response.data.data);
+      } else {
+        toast.error("Error fetching orders");
+      }
+    } catch (error) {
+      toast.error("Server error while fetching orders");
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllOrders();
+  }, []);
+
+  return (
+    <div className='order add'>
+      <h3>Order Page</h3>
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <div className="order-item" key={index}>
+            <img src={assets.parcel_icon} alt="Parcel Icon" />
+            <div>
+              <p className='order-item-food'>
+                {order.items.map(item => `${item.name} X ${item.quantity}`).join(', ')}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Orders;
